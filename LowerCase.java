e4729a877c06ac6762c2201e3509d46f5ce7a6b2
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:39:10 +0530

    Add files via upload

---
+++ LowerCase.java
@@ -0,0 +1,37 @@
+
+class LowerCase {
+ 
+  
+  public static void main(String[] args){
+
+ char ch = 'a';
+   
+   
+   if (ch>='a' && ch<='z') {        
+               
+
+ System.out.println(ch+" "+  "contain lower case alphabet");
+
+}
+    else{
+             System.out.println(ch+ "does not lower case alphabet");
+ 
+   }
+
+}
+}
+
+
+     
+
+
+
+                                                  
+
+
+
+
+
+
+
+
