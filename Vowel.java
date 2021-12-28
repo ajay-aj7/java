33b47fcd766e3ced60c08b53299c925769bfff11
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:41:52 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,27 @@
+class Vowel{
+ 
+  public static void main(String[] args){
+
+ char ch = 'c';
+
+  if (ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+  
+    System.out.println("vowel");
+}
+
+else{
+ 
+ System.out.println("consonant");
+}
+
+
+}
+}
+
+
+
+
+
+
+
+
