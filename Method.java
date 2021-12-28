d24a8ea3ecb048442750d55dc605b5eff498c5db
Ajay kattoju <96303899+ajay-aj7@users.noreply.github.com>
2021-12-28 21:39:27 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,21 @@
+class Method {
+  
+   public static void main(String[] args){
+  System.out.println("main..");
+     game();// method calling statment
+  System.out.println("iam ajay");
+     }
+  public static void game(){
+     tea();// method calling statment
+  System.out.println("football");
+        }
+public static void tea() {
+System.out.println("having tea");
+ }
+}
+
+
+
+
+
+
